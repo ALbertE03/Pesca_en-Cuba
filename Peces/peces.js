282aee7f93d689eb5097faf7f727b50e52a2421c
ALbertEO3 <marichalalberto292@gmail.com>
2024-08-02 17:58:22 -0400

    .

--- Peces/peces.js
+++ Peces/peces.js
@@ -1,38 +1,38 @@
 let imagenes = [
     {
-        "url": "/Pesca_en-Cubasrc/Almejas.jpg.webp",
+        "url": "/Pesca_en-Cuba/src/Almejas.jpg.webp",
         "nombre": "Proyecto 01",
         "descripcion": "aasadasdasd"
 
     },
     {
-        "url": "/Pesca_en-Cubasrc/Pargo.jpg",
+        "url": "/Pesca_en-Cuba/src/Pargo.jpg",
         "nombre": "Proyecto 02",
         "descripcion": "gdfgdfg"
 
     }, {
-        "url": "/Pesca_en-Cubasrc/Cherna.jpeg",
+        "url": "/Pesca_en-Cuba/src/Cherna.jpeg",
         "nombre": "Proyecto 02",
         "descripcion": "perrra"
 
     }, {
-        'url': "/Pesca_en-Cubasrc/tunidos.jpg",
+        'url': "/Pesca_en-Cuba/src/tunidos.jpg",
         'nombre': "Túnido",
         'descripcion': "Túnidos"
     }, {
-        'url': '/Pesca_en-Cubasrc/machuelos.PNG',
+        'url': '/Pesca_en-Cuba/src/machuelos.PNG',
         'nombre': 'Machuelos',
         'descripcion': 'pppppp'
     }, {
-        'url': '/Pesca_en-Cubasrc/claria.jpg',
+        'url': '/Pesca_en-Cuba/src/claria.jpg',
         'nombre': 'Claria',
         'descripcion': 'pppppp'
     }, {
-        'url': '/Pesca_en-Cubasrc/camaron.jpg',
+        'url': '/Pesca_en-Cuba/src/camaron.jpg',
         'nombre': 'Camaron',
         'descripcion': 'pppppp'
     }, {
-        'url': '/Pesca_en-Cubasrc/langosta.jpeg-2.webp',
+        'url': '/Pesca_en-Cuba/src/langosta.jpeg-2.webp',
         'nombre': 'Langosta',
         'descripcion': 'pppppp'
     }
