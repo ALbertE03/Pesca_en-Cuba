02bb2eb8e7d1fa3f72b4d6d5229c8a3b9b56b06c
ALbertEO3 <marichalalberto292@gmail.com>
2024-08-04 17:55:47 -0400

    .

--- Peces/peces.js
+++ Peces/peces.js
@@ -1,35 +1,40 @@
-let imagenes = [
-    {
-        "url": "/Pesca_en-Cuba/Peces/src/pargo-1.jpg",
-        "nombre": "Pargo",
-        "descripcion": "Empresa Pesca Caribe es una entidad que se destaca en el sector pesquero de Cuba, con presencia significativa en la industria. Se dirige principalmente a los sectores turístico y empresarial, ofreciendo productos y servicios que cumplen con las expectativas de un mercado exigente. Con un enfoque en la sostenibilidad y la responsabilidad ambiental, Pesca Caribe trabaja para proporcionar una variedad de productos pesqueros de alta calidad.La empresa se ha ganado una reputación por su compromiso con la excelencia y su capacidad para adaptarse a las cambiantes demandas del mercado."
+let imagenes = [{
+    "url": "/Pesca_en-Cuba/Peces/src/almeja-reyna-ing.jpg",
+    "nombre": "Almeja",
+    "descripcion": "Empresa Pesca Caribe es una entidad que se destaca en el sector pesquero de Cuba, con presencia significativa en la industria. Se dirige principalmente a los sectores turístico y empresarial, ofreciendo productos y servicios que cumplen con las expectativas de un mercado exigente. Con un enfoque en la sostenibilidad y la responsabilidad ambiental, Pesca Caribe trabaja para proporcionar una variedad de productos pesqueros de alta calidad.La empresa se ha ganado una reputación por su compromiso con la excelencia y su capacidad para adaptarse a las cambiantes demandas del mercado."
 
-    }, {
-        "url": "/Pesca_en-Cuba/Peces/src/grouper2.jpg",
-        "nombre": "Cherna",
-        "descripcion": "perrra"
+},
+{
+    "url": "/Pesca_en-Cuba/Peces/src/pargo-1.jpg",
+    "nombre": "Pargo",
+    "descripcion": "Empresa Pesca Caribe es una entidad que se destaca en el sector pesquero de Cuba, con presencia significativa en la industria. Se dirige principalmente a los sectores turístico y empresarial, ofreciendo productos y servicios que cumplen con las expectativas de un mercado exigente. Con un enfoque en la sostenibilidad y la responsabilidad ambiental, Pesca Caribe trabaja para proporcionar una variedad de productos pesqueros de alta calidad.La empresa se ha ganado una reputación por su compromiso con la excelencia y su capacidad para adaptarse a las cambiantes demandas del mercado."
 
-    }, {
-        'url': "/Pesca_en-Cuba/Peces/src/tunidos.webp",
-        'nombre': "Túnido",
-        'descripcion': "Túnidos"
-    }, {
-        'url': '/Pesca_en-Cuba/Peces/src/machuelo.jpg',
-        'nombre': 'Machuelos',
-        'descripcion': 'pppppp'
-    }, {
-        'url': '/Pesca_en-Cuba/Peces/src/88683_03.jpg',
-        'nombre': 'Claria',
-        'descripcion': 'pppppp'
-    }, {
-        'url': '/Pesca_en-Cuba/Peces/src/Comp-camaron-con-cabeza-ing.jpg',
-        'nombre': 'Camaron',
-        'descripcion': 'pppppp'
-    }, {
-        'url': '/Pesca_en-Cuba/Peces/src/compra-langosta-roja-de-Galicia.jpg',
-        'nombre': 'Langosta',
-        'descripcion': 'pppppp'
-    }
+}, {
+    "url": "/Pesca_en-Cuba/Peces/src/grouper2.jpg",
+    "nombre": "Cherna",
+    "descripcion": "perrra"
+
+}, {
+    'url': "/Pesca_en-Cuba/Peces/src/tunidos.webp",
+    'nombre': "Túnido",
+    'descripcion': "Túnidos"
+}, {
+    'url': '/Pesca_en-Cuba/Peces/src/machuelo.jpg',
+    'nombre': 'Machuelos',
+    'descripcion': 'pppppp'
+}, {
+    'url': '/Pesca_en-Cuba/Peces/src/88683_03.jpg',
+    'nombre': 'Claria',
+    'descripcion': 'pppppp'
+}, {
+    'url': '/Pesca_en-Cuba/Peces/src/Comp-camaron-con-cabeza-ing.jpg',
+    'nombre': 'Camaron',
+    'descripcion': 'pppppp'
+}, {
+    'url': '/Pesca_en-Cuba/Peces/src/compra-langosta-roja-de-Galicia.jpg',
+    'nombre': 'Langosta',
+    'descripcion': 'pppppp'
+}
 ]
 
 
