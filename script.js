7826435900e0daaebfd6d4d0b0d007673799c9a3
ALbertEO3 <marichalalberto292@gmail.com>
2024-08-05 14:08:29 -0400

    relax

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("Visitar").onclick = function () {
+    window.location.href = 'https://albert03.streamlit.app';
+};
+
+document.getElementById("leer").onclick = function () {
+    document.getElementById('mensaje').innerText = "hola";
+};
