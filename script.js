d647b701328720e512bf32ca36899f641fc5ccb6
ALbertEO3 <marichalalberto292@gmail.com>
2024-08-06 15:52:23 -0400

    .

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ document.getElementById("Visitar").onclick = function () {
 };
 
 document.getElementById("leer").onclick = function () {
-    document.getElementById('mensaje').innerText = "hola";
+    document.getElementById('mensaje').innerHTML = "<p> Es un proyecto de codigo abierto; lo puede encontar en <a target = '_blank' href='https://github.com/ALbertE03/data_product'> Repositorio</a>. <br>Todos los datos usados fueron extraidos de la <a target ='_blank' href='https://www.onei.gob.cu'> ONEI</a>.<br> Fue desplegado en los servicios de host de <a target='_blank' href='https://streamlit.io'>streamlit</a>, por lo que si lo usa desde Cuba tendrá que usar VPN.</p>";
+    document.getElementById('leer').style.display = 'none';
+    document.getElementById('menos').style.display = 'inline';
+
 };
+
+document.getElementById('menos').onclick = function () {
+    document.getElementById('mensaje').innerText = '';
+    document.getElementById('leer').style.display = 'inline';
+    document.getElementById('menos').style.display = 'none';
+};
+
